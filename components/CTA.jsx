faba821c45031356fb6fbb0e00ed0e1666159737
simonbeirouti <hello@simonbeirouti.com>
2022-10-14 12:03:26 +1100

    add regex and submit handler

--- components/CTA.jsx
+++ components/CTA.jsx
@@ -1,11 +1,46 @@
 import Image from "next/image";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 export default function CTA() {
+  const initValues = { email: "" };
+
   const [showForm, setShowForm] = useState(false);
+  const [formValues, setFormValues] = useState(initValues);
+  const [formErrors, setFormErrors] = useState({});
+  const [isSubmit, setIsSubmit] = useState(false);
 
   const handleClick = () => {
     setShowForm(!showForm);
+  };
+
+  const handleOnChange = (e) => {
+    const { name, value } = e.target;
+    setFormValues({ ...formValues, [name]: value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setFormErrors(validate(formValues));
+    setIsSubmit(true);
+    initValues.email = "";
+  };
+
+  useEffect(() => {
+    console.log(formErrors);
+    if (Object.keys(formErrors).length === 0 && isSubmit) {
+      console.log(formValues);
+    }
+  }, [formErrors]);
+
+  const validate = (values) => {
+    const errors = {};
+    const regEx = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/i;
+    if (!values.email) {
+      errors.email = "Email is required!";
+    } else if (!regEx.test(values.email)) {
+      errors.email = "This is not a valid email format!";
+    }
+    return errors;
   };
 
   return (
@@ -43,7 +78,7 @@ export default function CTA() {
                 Access now
               </button>
             ) : (
-              <form action="#" method="POST">
+              <form action="#" method="POST" onSubmit={handleSubmit}>
                 <button
                   type="submit"
                   className="inline-flex items-center justify-center px-6 py-4 text-base font-semibold leading-5 transition-all duration-200 bg-orange-300 border border-transparent rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-600 hover:bg-red-500 text-black"
@@ -52,17 +87,31 @@ export default function CTA() {
                 </button>
                 <div className="w-96">
                   <input
-                    type="email"
+                    type="text"
                     name="email"
-                    id="email"
-                    required="true"
+                    value={formValues.email}
+                    onChange={handleOnChange}
                     placeholder="Email address"
                     className="block w-full py-4 pl-4 mt-4 text-base text-gray-900 border border-gray-200 rounded-xl bg-gray-50 focus:bg-white focus:outline-none focus:border-blue-600 focus:ring-1 focus:ring-blue-600"
                   />
+
+                  {formErrors.email && (
+                    <p className="text-red-500 mt-4">{formErrors.email}</p>
+                  )}
                 </div>
               </form>
             )}
           </div>
+          {Object.keys(formErrors).length === 0 && isSubmit && (
+            <div
+              className="bg-green-100 mt-4 border border-green-400 text-green-700 px-4 py-3 rounded-md relative"
+              role="alert"
+            >
+              <span className="block sm:inline">
+                Success! Check your email for a magic link.
+              </span>
+            </div>
+          )}
         </div>
       </div>
     </section>
