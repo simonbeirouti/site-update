0701f61a608a861520f7819dde5295ccf7dab41a
simonbeirouti <hello@simonbeirouti.com>
2022-10-22 13:04:34 +1100

    add auth to _app and check for session

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,7 +1,29 @@
 import "../styles/globals.css";
+import { useState } from "react";
+import { createBrowserSupabaseClient } from "@supabase/auth-helpers-nextjs";
+import { SessionContextProvider } from "@supabase/auth-helpers-react";
+import { useSession } from "@supabase/auth-helpers-react";
+import Layout from "../components/Layout";
+import CTA from "../components/CTA";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  const [supabaseClient] = useState(() => createBrowserSupabaseClient());
+  const session = useSession();
+
+  return (
+    <SessionContextProvider
+      supabaseClient={supabaseClient}
+      initialSession={pageProps.initialSession}
+    >
+      {session ? (
+        <CTA />
+      ) : (
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      )}
+    </SessionContextProvider>
+  );
 }
 
 export default MyApp;
