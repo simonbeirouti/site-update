b2a83a059b026681924c7a032b90e2463c086ca2
simonbeirouti <hello@simonbeirouti.com>
2022-10-23 21:41:00 +1100

    change staging in meta to home

--- pages/index.jsx
+++ pages/index.jsx
@@ -10,7 +10,7 @@ export default function Home() {
   return (
     <>
       <MetaHead
-        title="Simon Beirouti | Staging"
+        title="Simon Beirouti | Home"
         description="This is the home page"
       />
       {!session ? (
