be42af61187a91a1a3dd264b397e895b32de8b3c
simonbeirouti <hello@simonbeirouti.com>
2022-10-23 19:50:07 +1100

    add session checker to page

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,19 +1,30 @@
 import MetaHead from "../components/utilities/MetaHead";
 import Hero from "../components/Hero";
 import Content from "../components/Content";
+import Header from "../components/utilities/Header";
+import AuthBox from "../components/utilities/AuthBox";
+import { useSession } from "@supabase/auth-helpers-react";
 
 export default function Home() {
+  const session = useSession();
   return (
     <>
       <MetaHead
         title="Simon Beirouti | Home"
         description="This is the home page"
       />
-      <Hero
-        intro="Welcome and thank you for visiting my website."
-        text="Recent updates for you below!"
-      />
-      <Content />
+      {!session ? (
+        <AuthBox />
+      ) : (
+        <>
+          <Header />
+          <Hero
+            intro="Welcome and thank you for visiting my website."
+            text="Recent updates for you below!"
+          />
+          <Content />
+        </>
+      )}
     </>
   );
 }
