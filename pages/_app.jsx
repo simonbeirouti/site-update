2c315b1bb668f9d02b38a7e6db580ca05d843ed9
simonbeirouti <hello@simonbeirouti.com>
2022-10-23 19:49:27 +1100

    rmv layout

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -2,26 +2,16 @@ import "../styles/globals.css";
 import { useState } from "react";
 import { createBrowserSupabaseClient } from "@supabase/auth-helpers-nextjs";
 import { SessionContextProvider } from "@supabase/auth-helpers-react";
-import { useSession } from "@supabase/auth-helpers-react";
-import Layout from "../components/utilities/Layout";
-import CTA from "../components/CTA";
 
 function MyApp({ Component, pageProps }) {
   const [supabaseClient] = useState(() => createBrowserSupabaseClient());
-  const session = useSession();
 
   return (
     <SessionContextProvider
       supabaseClient={supabaseClient}
       initialSession={pageProps.initialSession}
     >
-      {!session ? (
-        <CTA />
-      ) : (
-        <Layout>
-          <Component {...pageProps} />
-        </Layout>
-      )}
+      <Component {...pageProps} />
     </SessionContextProvider>
   );
 }
