fc599feadb7ae93f6cdefdf0121bf4696e4d7ffa
simonbeirouti <hello@simonbeirouti.com>
2022-10-25 08:31:49 +1100

    rmv home page content and card component

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,9 +1,8 @@
+import { useSession } from "@supabase/auth-helpers-react";
 import MetaHead from "../components/utilities/MetaHead";
 import Hero from "../components/Hero";
-import Content from "../components/Content";
 import Header from "../components/utilities/Header";
 import AuthBox from "../components/utilities/AuthBox";
-import { useSession } from "@supabase/auth-helpers-react";
 
 export default function Home() {
   const session = useSession();
@@ -18,11 +17,7 @@ export default function Home() {
       ) : (
         <>
           <Header />
-          <Hero
-            intro="Welcome and thank you for visiting my website."
-            text="Recent updates for you below!"
-          />
-          <Content />
+          <Hero intro="Lives updates" text="Recent updates for you below!" />
         </>
       )}
     </>
