2b603cc1116bf7291e221f87967f3488e1059cb4
simonbeirouti <hello@simonbeirouti.com>
2022-10-24 23:14:44 +1100

    add components and personalised content to 404

--- pages/404.jsx
+++ pages/404.jsx
@@ -1,6 +1,8 @@
 import MetaHead from "../components/utilities/MetaHead";
 import AuthBox from "../components/utilities/AuthBox";
 import { useSession } from "@supabase/auth-helpers-react";
+import Header from "../components/utilities/Header";
+import Hero from "../components/Hero";
 
 export default function ErrorPage() {
   const session = useSession();
@@ -10,7 +12,14 @@ export default function ErrorPage() {
         title="Simon Beirouti | 404"
         description="This is the 404 page. An error was made!"
       />
-      {!session && <AuthBox />}
+      {!session ? (
+        <AuthBox />
+      ) : (
+        <>
+          <Header />
+          <Hero intro="404" text="This page does not exist." />
+        </>
+      )}
     </>
   );
 }
