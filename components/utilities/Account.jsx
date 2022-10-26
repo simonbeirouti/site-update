9b3a4ab8f8c2412f6f47cef763fd1528195d5de8
simonbeirouti <hello@simonbeirouti.com>
2022-10-26 21:29:41 +1100

    create account component

---
+++ components/utilities/Account.jsx
@@ -0,0 +1,133 @@
+import { useState, useEffect } from "react";
+import { useUser, useSupabaseClient } from "@supabase/auth-helpers-react";
+
+export default function Account({ session }) {
+  const supabase = useSupabaseClient();
+  const user = useUser();
+  const [loading, setLoading] = useState(true);
+  const [username, setUsername] = useState(null);
+  const [website, setWebsite] = useState(null);
+  const [avatar_url, setAvatarUrl] = useState(null);
+
+  useEffect(() => {
+    getProfile();
+  }, [session]);
+
+  async function getProfile() {
+    try {
+      setLoading(true);
+
+      let { data, error, status } = await supabase
+        .from("profiles")
+        .select(`username, website, avatar_url`)
+        .eq("id", user.id)
+        .single();
+
+      if (error && status !== 406) {
+        throw error;
+      }
+
+      if (data) {
+        setUsername(data.username);
+        setWebsite(data.website);
+        setAvatarUrl(data.avatar_url);
+      }
+    } catch (error) {
+      alert("Error loading user data!");
+      console.log(error);
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  async function updateProfile({ username, website, avatar_url }) {
+    try {
+      setLoading(true);
+
+      const updates = {
+        id: user.id,
+        username,
+        website,
+        avatar_url,
+        updated_at: new Date().toISOString(),
+      };
+
+      let { error } = await supabase.from("profiles").upsert(updates);
+      if (error) throw error;
+      alert("Profile updated!");
+    } catch (error) {
+      alert("Error updating the data!");
+      console.log(error);
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  return (
+    <form className="max-w-xl mx-auto mt-12">
+      <div>
+        <label htmlFor="email" className="text-base font-medium text-gray-900">
+          Email
+        </label>
+        <input
+          id="email"
+          type="text"
+          value={user.email}
+          className="block w-full my-2 py-4 px-4 text-slate-400 cursor-not-allowed placeholder-gray-500 transition-all duration-200 bg-grey border border-gray-200 rounded-md focus:outline-none focus:border-blue-600 caret-blue-600"
+          disabled
+        />
+      </div>
+      <div>
+        <label
+          htmlFor="username"
+          className="text-base font-medium text-gray-900"
+        >
+          Username
+        </label>
+        <input
+          id="username"
+          type="text"
+          value={username || ""}
+          className="block w-full my-2 py-4 px-4 text-black cursor-pointer placeholder-gray-500 transition-all duration-200 bg-grey border border-gray-200 rounded-md focus:outline-none focus:border-blue-600 caret-blue-600"
+          onChange={(e) => setUsername(e.target.value)}
+        />
+      </div>
+      <div>
+        <label
+          htmlFor="website"
+          className="text-base font-medium text-gray-900"
+        >
+          Website
+        </label>
+        <input
+          id="website"
+          type="website"
+          value={website || ""}
+          className="block w-full my-2 py-4 px-4 text-black cursor-pointer placeholder-gray-500 transition-all duration-200 bg-grey border border-gray-200 rounded-md focus:outline-none focus:border-blue-600 caret-blue-600"
+          onChange={(e) => setWebsite(e.target.value)}
+        />
+      </div>
+
+      <div className="flex justify-evenly">
+        <div>
+          <button
+            className="inline-flex items-center justify-center px-6 py-4 text-base font-semibold leading-5 transition-all duration-200 bg-orange-300 border border-transparent rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-600 hover:bg-red-500 text-black"
+            onClick={() => updateProfile({ username, website, avatar_url })}
+            disabled={loading}
+          >
+            {loading ? "Loading ..." : "Update"}
+          </button>
+        </div>
+
+        <div>
+          <button
+            className="inline-flex items-center justify-center px-6 py-4 text-base font-semibold leading-5 transition-all duration-200 bg-orange-300 border border-transparent rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-600 hover:bg-red-500 text-black"
+            onClick={() => user.auth.signOut()}
+          >
+            Sign Out
+          </button>
+        </div>
+      </div>
+    </form>
+  );
+}
